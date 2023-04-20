c32258754e69e2fd4335eadd37af49df4d4605fa
Jennifer <jennifer.ngt@gmail.com>
2023-04-20 16:08:41 -0500

    third commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,19 @@
 import React from "react";
+import michiImg from "../michi-and-nori.jpeg"
 
 const About = () => {
   return (
     <div>
       <h1>About Page</h1>
-      <p>
+      <div className="container">
+      <img className="michi" src={michiImg} alt="shiba"/>
+     
+      <p className="aboutP">
         I was inspired to create this gallery because our family dog was a Shiba
         Inu. His name was Michi which means right path. He passed away suddenly this
         year. He was only 6 years old. RIP Michi. We love you.{" "}
       </p>
+      </div>
     </div>
   );
 };
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -2,14 +2,15 @@ import React from "react";
 import { useState, useEffect} from 'react';
 
 export default function Gallery() {
-   const [imgs, setImgs] = useState(0);
+   const [imgs, setImgs] = useState([]);
   const url = "http://shibe.online/api/shibes?count=100"
-  
+
   const getImgs = async () => {
     try {
       const response = await fetch(url)
       const data = await response.json()
       setImgs(data)
+      console.log(data)
     } catch (e) {
       console.error(e)
     }
@@ -22,20 +23,11 @@ export default function Gallery() {
      return (
      <div>
       <h1>Gallery</h1>
-      <img src={imgs[0]} alt="shiba"/>
-      <img src={imgs[1]} alt="shiba"/>
+      <div className="imgsDiv">
+      {imgs.map((img,index)=> ( <img src ={img} className="images" alt = "shiba" key={index}/>))};
+      </div>
       
-      {
-      const imgArr = imgs.map((img)=> {
-         return <img src ={imgs}/>
-      });
-      
-      }
-      
-      {/* {for (let i = 0, i<imgs.map, i++){
-         <img src={imgs[i]} alt="shiba"/>
-      }} */}
+      {/* <img src={imgs} alt="shiba"/> */}
 
       </div>
-     );
-  }
+      )}
